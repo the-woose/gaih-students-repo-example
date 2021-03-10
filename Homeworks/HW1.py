7606f57553f5f2349ede50e0e7f9c42dc75a51d0
the-woose <yavuzsava@yahoo.com.tr>
2021-03-11 02:08:47 +0300

    Homework 1

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,9 @@
-#Explain your work
-
+#-------------------------Homework 1-------------------------#
+max_range = 10; list_1 = list(range(max_range))
 #Question 1
-for x in range(a):
-	print(a)
+oddList = [i for i in list_1 if i%2==0]
+evenList = [i for i in list_1 if i%2==1]
+#Question 2
+mergedList = [i*2 for i in oddList] + [i*2 for i in evenList]
+#Question 3
+for i in mergedList: print(i)
