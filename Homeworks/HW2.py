a7d497dca50cff6aa7907b3ddac68fd175d6d84b
the-woose <yavuzsava@yahoo.com.tr>
2021-03-11 12:05:36 +0300

    Homework 2

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,18 @@
-#Explain your work
+#-------------------------Homework 2-------------------------#
+# isim kaynağı https://www.isimarsivi.com/isim-sihirbazi
 
 #Question 1
-for x in range(a):
-	print(a)
+CV1 = {'firstname':'Necib','lastname':'Ergül','age': 20,'position': 'CEO'}
+CV2 = {'firstname':'Rüstem','lastname':'Tokyay','age': 27,'position': 'HR'}
+CV3 = {'firstname':'İpek','lastname':'Tokyay','age': 25,'position': 'Intern'}
+CV4 = {'firstname':'Gizem','lastname':'Öztuna','age': 30,'position': 'CFO'}
+CV5 = {'firstname':'Alper','lastname':'Koyuncu','age': 22,'position': 'Intern'}
+
+#Question 2
+people = [CV1, CV2, CV3, CV4, CV5]	#----combine dictionaries
+for i in range(5):	#----"for" loop for selecting dictionary
+	CV = people[i]
+	print("Employee", i,"'s CV:")
+	for k,v in CV.items():	#----print items in dictionary
+		print(k,":",v)
+	print("\n")	#----seperate employee infos into blocks
