e0f4d0ec82707c315e1e4d6b9501244dca989c10
the-woose <yavuzsava@yahoo.com.tr>
2021-03-11 12:12:45 +0300

    Homework 2

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -12,7 +12,7 @@ CV5 = {'firstname':'Alper','lastname':'Koyuncu','age': 22,'position': 'Intern'}
 people = [CV1, CV2, CV3, CV4, CV5]	#----combine dictionaries
 for i in range(5):	#----"for" loop for selecting dictionary
 	CV = people[i]
-	print("Employee", i+1,"'s CV:")
+	print("Employee", i+1,"\b's CV:")
 	for k,v in CV.items():	#----print items in dictionary
-		print(k,":",v)
+		print(k,"\b:",v)
 	print("\n")	#----seperate employee infos into blocks
